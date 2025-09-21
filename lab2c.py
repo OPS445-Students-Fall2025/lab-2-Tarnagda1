30fd6f691d1de04f2562f134aa9a7d7c8bcebca8
Ibrahim Tarnagda <iytarnagda@myseneca.ca>
2025-09-21 10:40:33 -0400

    Added lab2c

---
+++ lab2c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# This program will display name and age 
+
+import sys
+
+name = sys.argv[1]
+age = sys.argv[2]
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
