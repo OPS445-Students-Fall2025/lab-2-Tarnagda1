2b9f5e74e8c89d359e22ec21433620f4ea85aceb
Ibrahim Tarnagda <iytarnagda@myseneca.ca>
2025-09-21 23:06:33 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author: Ibrahim Tarnagda
+
+# Author ID: iytarnagda
+
+# Date Created: 2025/09/21
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(str(timer))
+    timer = timer -1
+
+print("blast off!")
